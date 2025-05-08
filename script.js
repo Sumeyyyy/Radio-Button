96cd0fa2341a59904a218272963fab0133d01539
Sumeyyyy <23106118@srcas.ac.in>
2025-05-08 14:41:22 +0530

    script.js

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ else{
     alert(`HI ${name}, Your Selected Gender is ${selectedgender}.`);
     }
 });
+document.getElementById("cbutton").addEventListener("click",function(){
+    document.getElementById("Pname").value = "";
+    const selectedgender = document.getElementsByName("gender");
+    for(let option of selectedgender){
+        option.checked = false;
+    }
+});
